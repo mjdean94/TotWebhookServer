956c94484179e42a0414de94080a7b528ad9f1f2
Matthew Dean <mjdean1994@gmail.com>
2025-11-03 16:13:03 -0500

    updating with chat webhook endpoint

--- src/app.py
+++ src/app.py
@@ -13,9 +13,21 @@ async def root():
 @app.get("/tot")
 async def receive_tot_webhook(date, steamId, charName, actName, eventId, eventCategory, eventType, params):
 
-    log_entry = f"[{date}] Event={eventId}, Actor={charName}, Params=[{params}]"
+    log_entry = f"[{date}] EventType={eventType}, EventCategory={eventCategory} Actor={charName}, Params=[{params}]"
 
-    webhook = SyncWebhook.from_url(config.webhook_url)
+    webhook = SyncWebhook.from_url(config.audit_log_url)
     webhook.send(log_entry)
-    return {"message": "NYI"}
+    return "OK"
 
+@app.get("/chat")
+async def receive_tot_chat_logs(message, sender, character, radius, location, channel):
+    if sender != character:
+        actor = f"{sender} ({character})"
+    else:
+        actor = sender
+
+    log_entry = f"[{channel}][{radius}] {actor}: {message}"
+
+    webhook = SyncWebhook.from_url(config.chat_url)
+    webhook.send(log_entry)
+    return "OK"
