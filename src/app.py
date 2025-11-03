455c749d020bdf0c3bc36a9568296cc7441203c9
Matthew Dean <mjdean1994@gmail.com>
2025-11-03 16:04:08 -0500

    fixing webhook

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from discord import SyncWebhook
+import json
 
 from . import config
 
@@ -9,9 +10,12 @@ app = FastAPI()
 async def root():
     return "Tot Webhook Server is up and running."
 
-@app.post("/tot")
-async def receive_tot_webhook(payload):
+@app.get("/tot")
+async def receive_tot_webhook(date, steamId, charName, actName, eventId, eventCategory, eventType, params):
+
+    log_entry = f"[{date}] Event={eventId}, Actor={charName}, Params=[{params}]"
+
     webhook = SyncWebhook.from_url(config.webhook_url)
-    webhook.send(payload)
+    webhook.send(log_entry)
     return {"message": "NYI"}
 
